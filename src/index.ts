ffb6f47a14bb6dacad54f0135be0871a4fbf69ba
leliezer17 <leliezer123@gmail.com>
2023-04-28 17:08:11 -0400

    variables

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+let variable1: string = "Luis Arocha"
+const variable2: number = 2023
+let variable3: boolean= true
+let variable4: null | undefined = undefined
+let variable5: any = "jfjfjf"
+
+let variable6:  Array <any> = [
+    3,
+    5,
+    7,
+    0,
+    "Luis Arocha"
+]
+
+let variable7 = {
+    nombre: "Luis",
+    apellido: "Arocha",
+    profesion: "programador",
+    edad: "20",
+}
+
+console.log(variable7.profesion)
